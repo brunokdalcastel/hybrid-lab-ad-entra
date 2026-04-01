108b645a4516b978d2326a53e26513c1c531679e
Bruno Castel <brunokdalcastel@users.noreply.github.com>
2026-04-01 15:26:27 -0300

    feat: add outputs for resource IDs and names

---
+++ terraform/outputs.tf
@@ -0,0 +1,44 @@
+output "resource_group_name" {
+  description = "Name of the resource group"
+  value       = azurerm_resource_group.main.name
+}
+
+output "resource_group_id" {
+  description = "ID of the resource group"
+  value       = azurerm_resource_group.main.id
+}
+
+output "vnet_name" {
+  description = "Name of the hub virtual network"
+  value       = azurerm_virtual_network.hub.name
+}
+
+output "vnet_id" {
+  description = "ID of the hub virtual network"
+  value       = azurerm_virtual_network.hub.id
+}
+
+output "subnet_default_id" {
+  description = "ID of the default subnet"
+  value       = azurerm_subnet.default.id
+}
+
+output "nsg_id" {
+  description = "ID of the network security group"
+  value       = azurerm_network_security_group.main.id
+}
+
+output "route_table_id" {
+  description = "ID of the route table"
+  value       = azurerm_route_table.main.id
+}
+
+output "log_analytics_workspace_id" {
+  description = "ID of the Log Analytics workspace"
+  value       = azurerm_log_analytics_workspace.main.id
+}
+
+output "log_analytics_workspace_name" {
+  description = "Name of the Log Analytics workspace"
+  value       = azurerm_log_analytics_workspace.main.name
+}
