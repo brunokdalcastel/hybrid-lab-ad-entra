b4319d94acfd607d5bab544331498795dbb4181c
Bruno Castel <brunokdalcastel@users.noreply.github.com>
2026-04-02 13:28:27 -0300

    fix: marcar outputs com IDs como sensitive

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,6 +6,7 @@ output "resource_group_name" {
 output "resource_group_id" {
   description = "ID of the resource group"
   value       = azurerm_resource_group.main.id
+  sensitive   = true
 }
 
 output "vnet_name" {
@@ -16,26 +17,31 @@ output "vnet_name" {
 output "vnet_id" {
   description = "ID of the hub virtual network"
   value       = azurerm_virtual_network.hub.id
+  sensitive   = true
 }
 
 output "subnet_default_id" {
   description = "ID of the default subnet"
   value       = azurerm_subnet.default.id
+  sensitive   = true
 }
 
 output "nsg_id" {
   description = "ID of the network security group"
   value       = azurerm_network_security_group.main.id
+  sensitive   = true
 }
 
 output "route_table_id" {
   description = "ID of the route table"
   value       = azurerm_route_table.main.id
+  sensitive   = true
 }
 
 output "log_analytics_workspace_id" {
   description = "ID of the Log Analytics workspace"
   value       = azurerm_log_analytics_workspace.main.id
+  sensitive   = true
 }
 
 output "log_analytics_workspace_name" {
